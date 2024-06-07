c881b9400b588f12f6643dea789ae150c2851633
Julien Midedji <Julien.Midedji@gmail.com>
2024-06-07 14:07:39 +0200

    Fix bug in .xpi generation where old artefacts got bundled

--- scripts/xpi.js
+++ scripts/xpi.js
@@ -4,8 +4,16 @@ const { version } = require('../package.json');
 
 const archiver = require('archiver');
 
+const { execSync } = require('child_process');
 const fs = require('fs');
 
+// 1. Remove old stuff
+fs.rmSync('dist/', { recursive: true, force: true });
+
+// 2. Build dist folder
+execSync("npm run build");
+
+// 3. Bundle dist folder into xpi
 var output = fs.createWriteStream(`highlighter-${version}.xpi`);
 var archive = archiver('zip');
 
@@ -14,17 +22,15 @@ output.on('close', () => {
     console.log(`File 'highlighter-${version}.xpi' created`);
 });
 
-archive.on('error', (err) => {
-    throw err;
-});
-
 archive.on('warning', (err) => {
     throw err;
 });
 
+archive.on('error', (err) => {
+    throw err;
+});
+
+
 archive.pipe(output);
-
-// append files from a sub-directory, putting its contents at the root of archive
 archive.directory('dist/', false);
-
 archive.finalize();
